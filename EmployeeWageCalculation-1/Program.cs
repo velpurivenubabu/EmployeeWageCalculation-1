e2edd05cd6640d6c682c5431e98ea275d27ca3a7
venubabu <velpurivenubabu123@gmail.com>
2023-04-08 15:13:13 +0530

    [Venubabu] CalculatedEmployeeWage

--- EmployeeWageCalculation-1/Program.cs
+++ EmployeeWageCalculation-1/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             int isPresent = 1;
+            const int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
             //Welcome message
             Console.WriteLine("Welcome to Employee Wage Computation Problem Statement");
             //UC1-EmployeeAttandenance
@@ -13,11 +15,14 @@
             if (isPresent == empAttandence)
             {
                 Console.WriteLine("employee is prasent");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("employee is absent");
             }
+            int empWage = EMP_RATE_PER_HOUR * empHrs;
+            Console.WriteLine("Employee wage is : " + empWage);
         }
     }
 }
