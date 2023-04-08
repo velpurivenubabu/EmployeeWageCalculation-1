67a12501edd854cf2fa10bd94f38672c1baed495
venubabu <velpurivenubabu123@gmail.com>
2023-04-08 15:06:56 +0530

    [Venubabu] Modify Finding Employee Attendence

--- EmployeeWageCalculation-1/Program.cs
+++ EmployeeWageCalculation-1/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
+            int isPresent = 1;
+            //Welcome message
             Console.WriteLine("Welcome to Employee Wage Computation Problem Statement");
+            //UC1-EmployeeAttandenance
+            Random random = new Random();
+            int empAttandence = random.Next(0, 2);
+            if (isPresent == empAttandence)
+            {
+                Console.WriteLine("employee is prasent");
+            }
+            else
+            {
+                Console.WriteLine("employee is absent");
+            }
         }
     }
 }
