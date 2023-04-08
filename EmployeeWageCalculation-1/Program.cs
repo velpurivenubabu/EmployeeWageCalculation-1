40f672485e053747c1a7bc8c54b9bb8c8473d786
venubabu <velpurivenubabu123@gmail.com>
2023-04-08 16:29:37 +0530

    [Venubabu] Calculated Employee Wage for 20 Days

--- EmployeeWageCalculation-1/Program.cs
+++ EmployeeWageCalculation-1/Program.cs
@@ -4,32 +4,42 @@
     {
         static void Main(string[] args)
         {
+            int day = 0;
+            int empWage = 0;
+            int empHrs = 0;
             const int IS_FULL_TIME = 1;
             const int IS_PART_TIME = 2;
-            int isPresent = 1;
             const int EMP_RATE_PER_HOUR = 20;
-            int empHrs = 0;
+            int NUMBER_OF_WORKING_DAYS = 20;
+            int total_Wage = 0;
             //Welcome message
             Console.WriteLine("Welcome to Employee Wage Computation Problem Statement");
             //UC1-EmployeeAttandenance
             Random random = new Random();
-            int empAttandence = random.Next(0, 3);
-            switch (empAttandence)
+            for (day = 1; day <= NUMBER_OF_WORKING_DAYS; day++)
             {
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+
+                int empAttandence = random.Next(0, 3);
+                switch (empAttandence)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
 
 
+                }
+                empWage = EMP_RATE_PER_HOUR * empHrs;
+                Console.WriteLine("Employee wage for day {0} is {1}", day ,empWage );
+                total_Wage += empWage;
             }
-            int empWage = EMP_RATE_PER_HOUR * empHrs;
-            Console.WriteLine("Employee wage is : " + empWage);
+           
+            Console.WriteLine("Employee wage for 20 days is : " + total_Wage);
         }
     }
 }
