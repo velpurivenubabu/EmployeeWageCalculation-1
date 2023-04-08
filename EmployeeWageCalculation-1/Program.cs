c2873ce22e1a279c87ed7997cb821e6ef69f2a68
venubabu <velpurivenubabu123@gmail.com>
2023-04-08 15:20:38 +0530

    [Venubabu] Calculated Part Time Employee Wage Calculation

--- EmployeeWageCalculation-1/Program.cs
+++ EmployeeWageCalculation-1/Program.cs
@@ -4,6 +4,8 @@
     {
         static void Main(string[] args)
         {
+            const int IS_FULL_TIME = 1;
+            const int IS_PART_TIME = 2;
             int isPresent = 1;
             const int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
@@ -11,15 +13,20 @@
             Console.WriteLine("Welcome to Employee Wage Computation Problem Statement");
             //UC1-EmployeeAttandenance
             Random random = new Random();
-            int empAttandence = random.Next(0, 2);
-            if (isPresent == empAttandence)
+            int empAttandence = random.Next(0, 3);
+            if (IS_FULL_TIME == empAttandence)
             {
-                Console.WriteLine("employee is prasent");
+                Console.WriteLine("Fulltime employee is prasent");
                 empHrs = 8;
+            }
+            else if (IS_PART_TIME == empAttandence)
+            {
+                Console.WriteLine("Parttime employee is prasent");
+                empHrs = 4;
             }
             else
             {
-                Console.WriteLine("employee is absent");
+                Console.WriteLine("employee attence is absent");
             }
             int empWage = EMP_RATE_PER_HOUR * empHrs;
             Console.WriteLine("Employee wage is : " + empWage);
