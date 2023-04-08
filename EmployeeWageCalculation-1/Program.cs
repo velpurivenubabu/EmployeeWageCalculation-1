c81cd765cfaf704564c6a5298df6b328ad7a2846
venubabu <velpurivenubabu123@gmail.com>
2023-04-08 16:53:00 +0530

    [Venubabu] TotalWorkingHrs of employee is calculated

--- EmployeeWageCalculation-1/Program.cs
+++ EmployeeWageCalculation-1/Program.cs
@@ -4,21 +4,22 @@
     {
         static void Main(string[] args)
         {
-            int day = 0;
+            int day = 1;
+            int totalHrs = 0;
             int empWage = 0;
             int empHrs = 0;
             const int IS_FULL_TIME = 1;
             const int IS_PART_TIME = 2;
             const int EMP_RATE_PER_HOUR = 20;
             int NUMBER_OF_WORKING_DAYS = 20;
+            const int MAX_WORKING_HRS= 100;
             int total_Wage = 0;
             //Welcome message
             Console.WriteLine("Welcome to Employee Wage Computation Problem Statement");
             //UC1-EmployeeAttandenance
             Random random = new Random();
-            for (day = 1; day <= NUMBER_OF_WORKING_DAYS; day++)
+            while (day <= NUMBER_OF_WORKING_DAYS && totalHrs < MAX_WORKING_HRS)
             {
-
                 int empAttandence = random.Next(0, 3);
                 switch (empAttandence)
                 {
@@ -35,11 +36,13 @@
 
                 }
                 empWage = EMP_RATE_PER_HOUR * empHrs;
-                Console.WriteLine("Employee wage for day {0} is {1}", day ,empWage );
+                Console.WriteLine("Employee wage for day {0} and  {1} hrs is :{2} ", day, empHrs,empWage);
                 total_Wage += empWage;
+                day++;
+                totalHrs += empHrs;
             }
            
-            Console.WriteLine("Employee wage for 20 days is : " + total_Wage);
+            Console.WriteLine("TotalWage for {0} days and {1} hrs is {2}",(day-1),totalHrs,total_Wage);
         }
     }
 }
