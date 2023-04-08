5e69100023d1328769323e9ec6e2511aa94bd6f1
venubabu <velpurivenubabu123@gmail.com>
2023-04-08 15:01:35 +0530

    [Venu] Adding Welcome Message

---
+++ EmployeeWageCalculation-1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeWageCalculation_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Problem Statement");
+        }
+    }
+}
